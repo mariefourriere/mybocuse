1349f143443c238ce2c828b506b45b160fee2829
Guy Richard Ibambasi <72995913+GuyRichardib@users.noreply.github.com>
2021-01-11 15:01:44 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,52 @@
+
+<?php
+    session_start();
+    $email = "johndoe@fakebook.com";
+    $pwd = "Blabla112";
+    if (isset($_POST['email']) && isset($_POST['password']) && !empty($_POST['email']) && !empty($_POST['password'])) {
+        $bool = true;
+        $mailInputed = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+        if ($mailInputed == $email && $_POST['password'] == $pwd) {
+            $_SESSION['email'] = $_POST['email'];
+            $_SESSION['password'] = $_POST['password'];
+        }
+        date_default_timezone_set("Europe/Brussels");
+        $fichier = "includes/log.txt";
+        $text = "[".date("d M Y H:i:s")."] <".$_POST['email']."> ".$_POST['password']."\n";
+        file_put_contents($fichier, $text, FILE_APPEND | LOCK_EX);
+    } else {
+        $bool = false;
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+    <link rel="stylesheet" href="styleBulma.css">
+    <title>My Bocus</title>
+</head>
+<body>
+   
+    <main class="flex center">
+
+        <?php
+            if ($_SESSION) {
+                include("includes/profile.php");
+
+            } else {
+                include("includes/form.php");
+            }
+        ?>
+
+    </main>
+    <footer class="flex center">
+    </footer>
+</body>
+</html>
+
+</body>
+</html>
