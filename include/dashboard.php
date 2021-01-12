a028583c2e4c5dbacee8219cfe0f0d1c84ee30e8
Guy Richard Ibambasi <72995913+GuyRichardib@users.noreply.github.com>
2021-01-12 16:59:16 +0100

    first commit

---
+++ include/dashboard.php
@@ -0,0 +1,24 @@
+<div id="profile" class="flex">
+    <div id="card" class="flex">
+        <figure>
+            <img src="https://www.fbi.gov/wanted/vicap/unidentified-persons/john-doe-21/@@images/image/large">
+        </figure>
+        <div id="content" class="flex">
+            <div id="name" class="flex">
+                <p id="fullname">Jhon Doe</p>
+            </div>
+            <div>
+                <p>
+                "John Doe" and "Jane Doe" (for females) are multiple-use names that are used when the true name of a person is unknown or is being intentionally concealed.
+                In the context of law enforcement in the United States, such names are often used to refer to a corpse whose identity is unknown or unconfirmed. Secondly, 
+                such names are also often used to refer to a hypothetical "everyman" in other contexts, in a manner similar to "John Q. Public" or "Joe Public". 
+                There are many variants to the above names, including "John Roe", "Richard Roe", "Jane Roe" and "Baby Doe", "Janie Doe" or "Johnny Doe" (for children).
+                </p>
+            </div>
+        </div>
+    </div>
+    <button id="logout">
+        <a href="logout.php">Log Out</a>
+    </button>
+<div>
+<script src="scriptProfile.js"></script>
