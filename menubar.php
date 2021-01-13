c80f638a6f4cbf61a3253710e87afc3e6508e212
denababaie <72995019+denababaie@users.noreply.github.com>
2021-01-13 12:09:26 +0100

    changed path icons

--- menubar.php
+++ menubar.php
@@ -5,13 +5,13 @@
       <article class="tile is-child box has-background-black " id=menubar>
         <p class="title" id=titlemenu> my.</p>
         <figure class="homebutton">
-            <img src="./assets/icons/home.png">
+            <img src="./asset/icons/home.png">
       </figure>
       <figure class="calendarbutton">
-        <img src="./assets/icons/calendar.png">
+        <img src="./asset/icons/calendar.png">
      </figure>
     <figure class="logoutbutton">
-            <img src="./assets/icons/logout.png">
+            <img src="./asset/icons/logout.png">
       </figure>
       </article>
     </div>  
