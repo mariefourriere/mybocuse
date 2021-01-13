2b68938d8cb8d9df360310da4f3cbe70af82618a
denababaie <72995019+denababaie@users.noreply.github.com>
2021-01-13 12:05:35 +0100

    add calendar icon to menubar.php

--- menubar.php
+++ menubar.php
@@ -1,17 +1,17 @@
+<div class = studentdashboard>
 
-<!---------------------------------------MENU----DASHBOARD---------------------------------------------->
-
-    <div class = studentdashboard>
-
-    <div class="tile is-ancestor">
-        <div class="tile is-parent is-2" id=paddingmenu >
-          <article class="tile is-child box has-background-black " id=menubar>
-            <p class="title" id=titlemenu> my.</p>
-            <figure class="homebutton">
-                <img src="./asset/images/home.png">
-          </figure>
-            <figure class="logoutbutton">
-                <img src="./asset/images/logout.png">
-          </figure>
-          </article>
-        </div>
+<div class="tile is-ancestor">
+    <div class="tile is-parent is-2" id=paddingmenu >
+      <article class="tile is-child box has-background-black " id=menubar>
+        <p class="title" id=titlemenu> my.</p>
+        <figure class="homebutton">
+            <img src="./assets/icons/home.png">
+      </figure>
+      <figure class="calendarbutton">
+        <img src="./assets/icons/calendar.png">
+     </figure>
+    <figure class="logoutbutton">
+            <img src="./assets/icons/logout.png">
+      </figure>
+      </article>
+    </div>  
