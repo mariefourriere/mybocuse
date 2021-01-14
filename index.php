effb68d9d7b6677d859fe7aca85ba5f907b27a5a
Guy Richard Ibambasi <72995913+GuyRichardib@users.noreply.github.com>
2021-01-14 18:54:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -43,6 +43,10 @@ if (isset($_POST['email']) && isset($_POST['password']) && !empty($_POST['email'
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+	<script
+  src="https://code.jquery.com/jquery-3.5.1.min.js"
+  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+  crossorigin="anonymous"></script>
 
 </head>
 <body>
